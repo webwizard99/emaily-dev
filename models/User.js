7ebd717ed96b5fcf47267cdd1bab0ea4dfc5521b
Bezlonir <michael_bragg99@hotmail.com>
2019-01-28 16:26:29 -0700

    Complete OAuth flow.

---
+++ models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+// define the data types in the collection
+const userSchema = new Schema({
+  googleId: String
+});
+
+// create new collection
+mongoose.model('users', userSchema);
+
---
+++ services/passport.js
@@ -0,0 +1,39 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+// turn user into cookie
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// get user from cookie
+passport.deserializeUser((id, done)=> {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+  User.findOne({ googleId: profile.id })
+    .then((existingUser) => {
+      if (existingUser) {
+        // already have a record
+        done(null, existingUser);
+      } else {
+        // create a new user
+        new User({ googleId: profile.id })
+          .save()
+          .then(user => done(null, user));
+      }
+    });
+}));
+
