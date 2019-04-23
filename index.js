122b8a31db5c1607adefb3a6c19adfc44e643048
Bezlonir <michael_bragg99@hotmail.com>
2019-04-23 12:13:56 -0600

    Add console log to check process.env.

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
-
+console.dir(process.env);
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   // like our main.js file, or main.css file
